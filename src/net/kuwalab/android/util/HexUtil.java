abf8ce6bb14b7c3055c1d22f7d380887a9492c24
kuwalab <arata.shinichi@gmail.com>
2014-01-27 19:36:55 +0900

    HexUtilにJavaDocコメントを追加

--- src/net/kuwalab/android/util/HexUtil.java
+++ src/net/kuwalab/android/util/HexUtil.java
@@ -1,7 +1,19 @@
 package net.kuwalab.android.util;
 
-
+/**
+ * バイナリデータを扱うためのユーティリティ
+ * 
+ * @author kuwalab
+ * 
+ */
 public class HexUtil {
+	/**
+	 * byte配列を16進数文字列に変換する
+	 * 
+	 * @param bytes
+	 *            変換する配列
+	 * @return 変換後の文字列
+	 */
 	public static String toHexString(byte[] bytes) {
 		StringBuilder sb = new StringBuilder();
 
@@ -12,6 +24,13 @@ public class HexUtil {
 		return sb.toString();
 	}
 
+	/**
+	 * byte配列を16進数文字列配列に変換する
+	 * 
+	 * @param bytes
+	 *            変換する配列
+	 * @return 変換後の文字列配列
+	 */
 	public static String[] toHexStringArray(byte[] bytes) {
 		String[] result = new String[bytes.length];
 
@@ -22,6 +41,13 @@ public class HexUtil {
 		return result;
 	}
 
+	/**
+	 * byte変数を16進数文字列に変換する
+	 * 
+	 * @param oneByte
+	 *            変換する変数
+	 * @return 変換後の文字列
+	 */
 	public static String toHexString(byte oneByte) {
 		StringBuilder sb = new StringBuilder();
 
@@ -36,6 +62,13 @@ public class HexUtil {
 		return sb.toString();
 	}
 
+	/**
+	 * byte配列を連続したバイナリとみなし、数値に変換する
+	 * 
+	 * @param bytes
+	 *            変換する配列
+	 * @return 変換後の数値
+	 */
 	public static int toInt(byte[] bytes) {
 		int result = 0;
 		int base = 1;
@@ -48,6 +81,13 @@ public class HexUtil {
 		return result;
 	}
 
+	/**
+	 * byte変数を符号なし整数とみなしint型に変換する
+	 * 
+	 * @param b
+	 *            変換する変数
+	 * @return 変換後の整数
+	 */
 	public static int toInt(byte b) {
 		int result = b;
 
