b205f704c6eed83294a2fdd3b87fdfd8a9318e05
kuwalab <arata.shinichi@gmail.com>
2014-07-05 16:21:36 +0900

    fix # 28 重複している定数の除去

--- ICaReader/src/main/java/net/kuwalab/android/icareader/ICaWidget.java
+++ ICaReader/src/main/java/net/kuwalab/android/icareader/ICaWidget.java
@@ -15,12 +15,8 @@ import android.widget.RemoteViews;
  * @author kuwalab
  */
 public class ICaWidget extends AppWidgetProvider {
-    protected static final String PREFERENCES_NAME = "ICA_DATA";
-    protected static final String PREFERENCES_CONF_DATE = "conf_date";
-    protected static final String PREFERENCES_REST_MONEY = "rest_money";
-
     public void onUpdate(Context context, AppWidgetManager awm, int[] ids) {
-        SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME,
+        SharedPreferences pref = context.getSharedPreferences(HistoryFragment.PREFERENCES_NAME,
                 Context.MODE_PRIVATE);
         ICaWidget.update(context, awm, pref);
     }
@@ -32,12 +28,12 @@ public class ICaWidget extends AppWidgetProvider {
                 R.layout.widget_main);
         remoteViews.setTextViewText(
                 R.id.confDate,
-                pref.getString(PREFERENCES_CONF_DATE,
+                pref.getString(HistoryFragment.PREFERENCES_CONF_DATE,
                         res.getString(R.string.ica_widget_unconfirmed))
         );
         remoteViews.setTextViewText(
                 R.id.widetRest,
-                pref.getString(PREFERENCES_REST_MONEY,
+                pref.getString(HistoryFragment.PREFERENCES_REST_MONEY,
                         res.getString(R.string.ica_widget_default_rest_money))
         );
         awm.updateAppWidget(new ComponentName(context, ICaWidget.class),
