260d0961c8c28323eedba04e6efc1969012979d1
kuwalab <arata.shinichi@gmail.com>
2014-07-10 13:01:02 +0900

    importの編成

--- ICaReader/src/main/java/net/kuwalab/android/icareader/MainActivity.java
+++ ICaReader/src/main/java/net/kuwalab/android/icareader/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.widget.TextView;
 
 /**
  * カードを読み取るメインクラス
