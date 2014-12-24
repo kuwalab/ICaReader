3f6afa349c9453a9143fe98308cc5e6b3fc940c0
kuwalab <arata.shinichi@gmail.com>
2014-12-24 18:41:21 +0900

    HexUtilのテストをJUnit4に変更

--- app/src/androidTest/java/net/kuwalab/android/util/HexUtilTest.java
+++ app/src/androidTest/java/net/kuwalab/android/util/HexUtilTest.java
@@ -1,8 +1,15 @@
 package net.kuwalab.android.util;
 
-import junit.framework.TestCase;
+import android.support.test.runner.AndroidJUnit4;
 
-public class HexUtilTest extends TestCase {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class HexUtilTest {
     private static final byte x0_b0000_0000 = 0;
     private static final byte x1_b0000_0001 = 1;
     private static final byte x15_b0000_1111 = 15;
@@ -17,7 +24,7 @@ public class HexUtilTest extends TestCase {
             sb.append(b).append(" ");
         }
         sb.append(")=").append(expected);
-        assertEquals(sb.toString(), expected, HexUtil.toHexString(input));
+        assertThat(sb.toString(), HexUtil.toHexString(input), is(expected));
     }
 
     private void assert16進数文字列チェック(byte[] input, String[] expected) {
@@ -33,7 +40,7 @@ public class HexUtilTest extends TestCase {
 
         String[] actual = HexUtil.toHexStringArray(input);
         for (int i = 0; i < actual.length; i++) {
-            assertEquals(sb.toString(), expected[i], actual[i]);
+            assertThat(sb.toString(), actual[i], is(expected[i]));
         }
     }
 
@@ -44,18 +51,20 @@ public class HexUtilTest extends TestCase {
             sb.append(b).append(" ");
         }
         sb.append(")=").append(expected);
-        assertEquals(sb.toString(), expected, HexUtil.toInt(input));
+        assertThat(sb.toString(), HexUtil.toInt(input), is(expected));
     }
 
+    @Test
     public void testToHexString() {
-        assertEquals("00", HexUtil.toHexString(x0_b0000_0000));
-        assertEquals("01", HexUtil.toHexString(x1_b0000_0001));
-        assertEquals("0f", HexUtil.toHexString(x15_b0000_1111));
-        assertEquals("10", HexUtil.toHexString(x16_b0001_0000));
-        assertEquals("80", HexUtil.toHexString(xminus128_b1000_0000));
-        assertEquals("ff", HexUtil.toHexString(xminus1_1111_1111));
+        assertThat(HexUtil.toHexString(x0_b0000_0000), is("00"));
+        assertThat(HexUtil.toHexString(x1_b0000_0001), is("01"));
+        assertThat(HexUtil.toHexString(x15_b0000_1111), is("0f"));
+        assertThat(HexUtil.toHexString(x16_b0001_0000), is("10"));
+        assertThat(HexUtil.toHexString(xminus128_b1000_0000), is("80"));
+        assertThat(HexUtil.toHexString(xminus1_1111_1111), is("ff"));
     }
 
+    @Test
     public void testToHexString2桁() {
         assert16進数文字列チェック(new byte[]{x0_b0000_0000}, "00");
         assert16進数文字列チェック(new byte[]{x1_b0000_0001}, "01");
@@ -65,6 +74,7 @@ public class HexUtilTest extends TestCase {
         assert16進数文字列チェック(new byte[]{xminus1_1111_1111}, "ff");
     }
 
+    @Test
     public void testToHexString4桁() {
         assert16進数文字列チェック(new byte[]{x0_b0000_0000, x1_b0000_0001}, "0001");
         assert16進数文字列チェック(new byte[]{x15_b0000_1111, x16_b0001_0000}, "0f10");
@@ -74,6 +84,7 @@ public class HexUtilTest extends TestCase {
                 "8010");
     }
 
+    @Test
     public void testToHexStringArray2桁() {
         assert16進数文字列チェック(new byte[]{x0_b0000_0000}, new String[]{"00"});
         assert16進数文字列チェック(new byte[]{x1_b0000_0001}, new String[]{"01"});
@@ -85,6 +96,7 @@ public class HexUtilTest extends TestCase {
                 new String[]{"ff"});
     }
 
+    @Test
     public void testToHexStringArray4桁() {
         assert16進数文字列チェック(new byte[]{x0_b0000_0000, x1_b0000_0001},
                 new String[]{"00", "01"});
@@ -96,15 +108,17 @@ public class HexUtilTest extends TestCase {
                 new String[]{"80", "10"});
     }
 
+    @Test
     public void testToInt() {
-        assertEquals(0, HexUtil.toInt(x0_b0000_0000));
-        assertEquals(1, HexUtil.toInt(x1_b0000_0001));
-        assertEquals(15, HexUtil.toInt(x15_b0000_1111));
-        assertEquals(16, HexUtil.toInt(x16_b0001_0000));
-        assertEquals(128, HexUtil.toInt(xminus128_b1000_0000));
-        assertEquals(255, HexUtil.toInt(xminus1_1111_1111));
+        assertThat(HexUtil.toInt(x0_b0000_0000), is(0));
+        assertThat(HexUtil.toInt(x1_b0000_0001), is(1));
+        assertThat(HexUtil.toInt(x15_b0000_1111), is(15));
+        assertThat(HexUtil.toInt(x16_b0001_0000), is(16));
+        assertThat(HexUtil.toInt(xminus128_b1000_0000), is(128));
+        assertThat(HexUtil.toInt(xminus1_1111_1111), is(255));
     }
 
+    @Test
     public void testToIntArray1桁() {
         assertIntチェック(new byte[]{x0_b0000_0000}, 0);
         assertIntチェック(new byte[]{x1_b0000_0001}, 1);
@@ -114,6 +128,7 @@ public class HexUtilTest extends TestCase {
         assertIntチェック(new byte[]{xminus1_1111_1111}, 255);
     }
 
+    @Test
     public void testToIntArray2桁() {
         assertIntチェック(new byte[]{x0_b0000_0000, x1_b0000_0001}, 1);
         assertIntチェック(new byte[]{x1_b0000_0001, x1_b0000_0001}, 257);
