43a2e7ffef817e5af497a176ab3c6f2dbaeee07b
kuwalab <arata.shinichi@gmail.com>
2015-07-31 13:03:25 +0900

    ライセンスをアプリの中で明示する

--- app/src/main/java/net/kuwalab/android/icareader/MainActivity.java
+++ app/src/main/java/net/kuwalab/android/icareader/MainActivity.java
@@ -1,12 +1,18 @@
 package net.kuwalab.android.icareader;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * カードを読み取るメインクラス
@@ -44,4 +50,39 @@ public class MainActivity extends ActionBarActivity {
         setIntent(new Intent());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_lisence) {
+            final Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.webview_lisence);
+            dialog.setTitle("オープンソースライセンス");
+            dialog.setCancelable(true);
+            WebView webView = (WebView)dialog.findViewById(R.id.webview);
+
+            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    dialog.show();
+                }
+            });
+            webView.loadUrl("file:///android_asset/lisence.html");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
