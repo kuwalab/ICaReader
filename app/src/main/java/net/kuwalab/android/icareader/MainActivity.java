0e182c5bfb5d651ea1623df9c935ed5f5569672f
kuwalab <arata.shinichi@gmail.com>
2015-08-01 10:47:32 +0900

    DialogFragmentに変更

--- app/src/main/java/net/kuwalab/android/icareader/MainActivity.java
+++ app/src/main/java/net/kuwalab/android/icareader/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -60,39 +62,27 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_lisence) {
-            showDialog(LISENCE_DIALOG_ID);
+            FragmentManager manager = getSupportFragmentManager();
+            LisenceDialogFragment d = new LisenceDialogFragment();
+            d.show(manager, "dialog");
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    /**showDialogが呼ばれたら実行される*/
-    public Dialog onCreateDialog(int id) {
-        Dialog dialog = null;
-        switch (id) {
-            case LISENCE_DIALOG_ID:
-                dialog = new Dialog(this);
-                dialog.setContentView(R.layout.webview_lisence);
-                dialog.setTitle("オープンソースライセンス");
-                dialog.setCancelable(true);
-                WebView webView = (WebView) dialog.findViewById(R.id.webViewLisence);
+    public static class LisenceDialogFragment extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Dialog dialog = new Dialog(getActivity());
+            dialog.setContentView(R.layout.webview_lisence);
+            dialog.setTitle("オープンソースライセンス");
+            dialog.setCancelable(true);
+            WebView webView = (WebView) dialog.findViewById(R.id.webViewLisence);
 
-                webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-                webView.loadUrl("file:///android_asset/lisence.html");
-                break;
-        }
-
-        return dialog;
-    }
-
-    @Override
-    public void onPrepareDialog(int id, Dialog dialog) {
-        super.onPrepareDialog(id, dialog);
-
-        switch (id) {
-            case LISENCE_DIALOG_ID:
-                break;
+            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+            webView.loadUrl("file:///android_asset/lisence.html");
+            return dialog;
         }
     }
 }
